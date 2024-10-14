afaedb687dd17fa33ce4878abdbdd2d1a1ce9bdf
amit sunda <amitsunda14374@gmail.com>
2024-10-14 15:44:58 +0530

    first

--- js/index-script.js
+++ js/index-script.js
@@ -7,19 +7,19 @@ $(window).load(function() {
 
 
     /* reshuffle when user clicks a filter item */
-    $('#filter a').click(function (e) {
-        e.preventDefault();
+    // $('#filter a').click(function (e) {
+    //     e.preventDefault();
 
-        // set active class
-        $('#filter a').removeClass('active');
-        $(this).addClass('active');
+    //     // set active class
+    //     $('#filter a').removeClass('active');
+    //     $(this).addClass('active');
 
-        // get group name from clicked item
-        var groupName = $(this).attr('data-group');
+    //     // get group name from clicked item
+    //     var groupName = $(this).attr('data-group');
 
-        // reshuffle grid
-        $main.shuffle('shuffle', groupName );
-    });
+    //     // reshuffle grid
+    //     $main.shuffle('shuffle', groupName );
+    // });
 });
 
 
